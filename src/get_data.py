5cb23737c712ac83aee3da82fc32b29ce0643305
Nitin Verma <nitfver@gmail.com>
2021-05-30 17:24:53 +0200

    Stage 1 Completed

--- src/get_data.py
+++ src/get_data.py
@@ -18,7 +18,7 @@ def get_data(config_path):
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
     #print(df.head())
     return df
-
+##extra comments added
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
